0928c40aff66483207c19077603472ca9ce7f831
SergSedoy <78666743+SergSedoy@users.noreply.github.com>
2021-05-31 18:58:10 +0300

    Update Main.java

--- src/main/java/task2/Main.java
+++ src/main/java/task2/Main.java
@@ -5,14 +5,22 @@ import java.util.concurrent.atomic.LongAdder;
 import java.util.stream.IntStream;
 
 public class Main {
+    public static LongAdder stat = new LongAdder();
+    
     public static void main(String[] args) throws InterruptedException {
-        ExecutorService executorService =
-                Executors.newFixedThreadPool(3);
-        LongAdder stat = new LongAdder();
-        IntStream.range(0, 100)
-                .forEach(i -> executorService.submit(() -> stat.add(i)));
-        executorService.awaitTermination(3, TimeUnit.SECONDS);
+
+        User th1 = new User("первый поток");
+        User th2 = new User("второй поток");
+        User th3 = new User("третий поток");
+
+        th1.start();
+        th2.start();
+        th3.start();
+
+        th1.join();
+        th2.join();
+        th3.join();
+
         System.out.println("\nРезультат: " + stat.sum());
-        executorService.shutdown();
     }
 }
